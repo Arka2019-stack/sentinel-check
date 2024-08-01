bf9da11b50a2a189fc1ccc49cb55041360ec8fd6
Arka Chakraborty <rikcarka@gmail.com>
2024-08-01 16:41:26 +0530

    instance creation

---
+++ instance.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my-app" {
+
+  ami = "ami-0427090fd1714168b"
+  subnet_id = "subnet-0edd425aeb259bc23"
+  instance_type = "t2.micro"
+  
+}
